d9505f94a1f748f1caffac9f368d1a3e066e7f3e
Atul R <atulanand94@gmail.com>
2019-06-17 22:42:48 +0200

    Added qode version on process.version

--- src/qode.cc
+++ src/qode.cc
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+std::string qodeVersion = "1.0.0-alpha";
+
 namespace qode {
   static int qode_argc = 0;
   static char** qode_argv = nullptr;
@@ -14,7 +16,22 @@ namespace qode {
   // Has we run message loop before.
   bool g_first_runloop = true;
 
+  inline v8::Local<v8::String> ToV8String(node::Environment* env, const std::string str) {
+   return v8::String::NewFromUtf8(
+      env->isolate(), str.c_str(), v8::NewStringType::kNormal, static_cast<int>(str.length())
+    ).ToLocalChecked();
+  }
+
   bool InitWrapper(node::Environment* env) {
+    v8::HandleScope handle_scope(env->isolate());
+    v8::Local<v8::Value> versions = env->process_object()->Get(
+        env->context(), ToV8String(env, "versions")
+    ).ToLocalChecked();
+    versions.As<v8::Object>()->Set(
+      env->context(),
+      ToV8String(env, "qode"),
+      ToV8String(env, qodeVersion)
+    ).ToChecked();
     return true;
   }
 
@@ -41,19 +58,15 @@ namespace qode {
     // Prepare node integration.
     g_node_integration.reset(NodeIntegration::Create());
     g_node_integration->Init();
-
     // Set run loop and init function on node.
     node::InjectQode(&InitWrapper, &RunLoopWrapper);
-    
     // Always enable GC this app is almost always running on desktop.
     v8::V8::SetFlagsFromString("--expose_gc", 11);
-
     int code = node::Start(qode_argc, qode_argv);
-
     // Clean up node integration and quit.
     g_node_integration.reset();
     return code;
   }
 
-}  // namespace qode
+}  
 

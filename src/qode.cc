8524ee51532cad98ced84b49f77226f9b967f0e2
master-atul <atulanand94@gmail.com>
2019-08-16 00:04:00 +0200

    bump version

--- src/qode.cc
+++ src/qode.cc
@@ -7,84 +7,87 @@
 #include <stdlib.h>
 #include <iostream>
 
-std::string qodeVersion = "1.0.0";
+std::string qodeVersion = "1.0.1";
 
-namespace qode {
-  QApplication* qtAppInstance = nullptr;
-  static int qode_argc = 0;
-  static char** qode_argv = nullptr;
-  // The global instance of NodeIntegration.
-  std::unique_ptr<NodeIntegration> g_node_integration;
-  // Has we run message loop before.
-  bool g_first_runloop = true;
+namespace qode
+{
+QApplication *qtAppInstance = nullptr;
+static int qode_argc = 0;
+static char **qode_argv = nullptr;
+// The global instance of NodeIntegration.
+std::unique_ptr<NodeIntegration> g_node_integration;
+// Has we run message loop before.
+bool g_first_runloop = true;
 
-  inline v8::Local<v8::String> ToV8String(node::Environment* env, const std::string str) {
-   return v8::String::NewFromUtf8(
-      env->isolate(), str.c_str(), v8::NewStringType::kNormal, static_cast<int>(str.length())
-    ).ToLocalChecked();
+inline v8::Local<v8::String> ToV8String(node::Environment *env, const std::string str)
+{
+  return v8::String::NewFromUtf8(
+             env->isolate(), str.c_str(), v8::NewStringType::kNormal, static_cast<int>(str.length()))
+      .ToLocalChecked();
+}
+
+bool InitWrapper(node::Environment *env)
+{
+  v8::HandleScope handle_scope(env->isolate());
+  v8::Local<v8::Value> versions = env->process_object()->Get(
+                                                           env->context(), ToV8String(env, "versions"))
+                                      .ToLocalChecked();
+  versions.As<v8::Object>()->Set(
+                               env->context(),
+                               ToV8String(env, "qode"),
+                               ToV8String(env, qodeVersion))
+      .ToChecked();
+
+  versions.As<v8::Object>()->Set(
+                               env->context(),
+                               ToV8String(env, "qt(compiled)"),
+                               ToV8String(env, QT_VERSION_STR))
+      .ToChecked();
+
+  versions.As<v8::Object>()->Set(
+                               env->context(),
+                               ToV8String(env, "qt(runtime)"),
+                               ToV8String(env, qVersion()))
+      .ToChecked();
+
+  return true;
+}
+
+bool RunLoopWrapper(node::Environment *env)
+{
+  // Run uv loop for once before entering GUI message loop.
+  if (g_first_runloop)
+  {
+    g_node_integration->UvRunOnce();
+    g_first_runloop = false;
   }
+  int exitCode = qtAppInstance->exec();
+  std::cout << "Qt exited with " << exitCode;
+  exit(exitCode);
+  // No need to keep uv loop alive.
+  // g_node_integration->ReleaseHandleRef();
+  // Enter uv loop to handle unfinished uv tasks.
+  // return uv_run(env->event_loop(), UV_RUN_DEFAULT);
+}
 
-  bool InitWrapper(node::Environment* env) {
-    v8::HandleScope handle_scope(env->isolate());
-    v8::Local<v8::Value> versions = env->process_object()->Get(
-        env->context(), ToV8String(env, "versions")
-    ).ToLocalChecked();
-    versions.As<v8::Object>()->Set(
-      env->context(),
-      ToV8String(env, "qode"),
-      ToV8String(env, qodeVersion)
-    ).ToChecked();
+int Start(int argc, char *argv[])
+{
+  qode_argc = argc;
+  qode_argv = argv;
 
-    versions.As<v8::Object>()->Set(
-      env->context(),
-      ToV8String(env, "qt(compiled)"),
-      ToV8String(env, QT_VERSION_STR)
-    ).ToChecked();
+  qtAppInstance = new QApplication(qode_argc, qode_argv);
+  qtAppInstance->processEvents(); // Just run it once.
+  // Prepare node integration.
+  g_node_integration.reset(NodeIntegration::Create());
+  g_node_integration->Init();
+  // Set run loop and init function on node.
+  node::InjectQode(&InitWrapper, &RunLoopWrapper);
+  // Always enable GC this app is almost always running on desktop.
+  v8::V8::SetFlagsFromString("--expose_gc", 11);
+  int code = node::Start(qode_argc, qode_argv);
+  // Clean up node integration and quit.
+  g_node_integration.reset();
+  return code;
+}
 
-    
-    versions.As<v8::Object>()->Set(
-      env->context(),
-      ToV8String(env, "qt(runtime)"),
-      ToV8String(env, qVersion())
-    ).ToChecked();
-
-    return true;
-  }
-
-  bool RunLoopWrapper(node::Environment* env) {
-    // Run uv loop for once before entering GUI message loop.
-    if (g_first_runloop) {
-      g_node_integration->UvRunOnce();
-      g_first_runloop = false;
-    }
-    int exitCode = qtAppInstance->exec();
-    std::cout << "Qt exited with "<< exitCode;
-    exit(exitCode);
-    // No need to keep uv loop alive.
-    // g_node_integration->ReleaseHandleRef();
-    // Enter uv loop to handle unfinished uv tasks.
-    // return uv_run(env->event_loop(), UV_RUN_DEFAULT);
-  }
-
-
-  int Start(int argc, char* argv[]) {
-    qode_argc = argc;
-    qode_argv = argv;
-
-    qtAppInstance = new QApplication(qode_argc, qode_argv);
-    qtAppInstance->processEvents(); // Just run it once.
-    // Prepare node integration.
-    g_node_integration.reset(NodeIntegration::Create());
-    g_node_integration->Init();
-    // Set run loop and init function on node.
-    node::InjectQode(&InitWrapper, &RunLoopWrapper);
-    // Always enable GC this app is almost always running on desktop.
-    v8::V8::SetFlagsFromString("--expose_gc", 11);
-    int code = node::Start(qode_argc, qode_argv);
-    // Clean up node integration and quit.
-    g_node_integration.reset();
-    return code;
-  }
-
-}  
-
+} // namespace qode

963856472459228a57ea3e9712b260b0f4c93a06
Atul <atulanand94@gmail.com>
2019-08-17 01:58:27 +0200

    Fix linux build

--- deploy/linux/postbuild.sh
+++ deploy/linux/postbuild.sh
@@ -13,6 +13,8 @@ cp -rf $QT_INSTALL_DIR/lib/libQt5Core.so* ./linux/lib/
 cp -rf $QT_INSTALL_DIR/lib/libQt5Gui.so* ./linux/lib/
 cp -rf $QT_INSTALL_DIR/lib/libQt5Widgets.so* ./linux/lib/
 cp -rf $QT_INSTALL_DIR/lib/libicu* ./linux/lib/
+cp -rf $QT_INSTALL_DIR/lib/libQt5DBus.so* ./linux/lib/
+cp -rf $QT_INSTALL_DIR/lib/libQt5XcbQpa.so* ./linux/lib/
 
 mkdir -p ./linux/include
 
@@ -24,8 +26,10 @@ echo "Copying plugins"
 mkdir -p ./linux/plugins
 cp -R "$QT_INSTALL_DIR/plugins/iconengines" "./linux/plugins/iconengines"
 cp -R "$QT_INSTALL_DIR/plugins/imageformats" "./linux/plugins/imageformats"
-cp -R "$QT_INSTALL_DIR/plugins/platforms" "./linux/plugins/platforms"
 cp -R "$QT_INSTALL_DIR/plugins/platformthemes" "./linux/plugins/platformthemes"
+cp -R "$QT_INSTALL_DIR/plugins/platforms" "./linux/platforms"
+# patchelfing platforms so that they can find right libs
+patchelf --set-rpath "\$ORIGIN/../lib" ./linux/platforms/libqxcb.so
 
 cp ./qode ./linux/qode
 
@@ -34,5 +38,7 @@ cd ./linux
 strip qode
 
 patchelf --set-rpath "\$ORIGIN/lib" qode
+
+
 
 echo "Qode is ready!"

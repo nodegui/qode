d304ab6db4d026300a7f37493d2c606b304e71fd
Atul R <atulanand94@gmail.com>
2020-02-22 10:47:52 +0100

    Fix await on fsExist

--- npm/src/setup.js
+++ npm/src/setup.js
@@ -10,13 +10,15 @@ const fsExist = util.promisify(fs.exists);
 
 async function setupQode() {
   const { extractDir, cacheDir, downloadLink } = config;
-  if (fsExist(config.qodePath)) {
+  if (await fsExist(config.qodePath)) {
+    console.log(`Qode binary exists at ${config.qodePath}, skipping setup.`);
     return extractDir;
   }
   const archivePath = path.resolve(cacheDir, path.basename(downloadLink));
   await mkdirp(extractDir);
   await download(downloadLink, archivePath, true);
   await extract(archivePath, extractDir);
+  fs.chmodSync(config.qodePath, 0o775);
   console.log(`Qode was setup successfully at : ${extractDir}`);
   return extractDir;
 }
